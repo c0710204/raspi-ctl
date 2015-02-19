124a5fa263336deb6c40e758c36c2e503db2ac2a
顾翔 <c0710204@gmail.com>
2015-02-19 20:00:42 +0800

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='raspi-ctl',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Xiang Gu',
+    author_email='c0710204@gmail.com',
+    description=''
+)
